466dfd94d8d2880beee12e0ef5ff56254f9d0318
Eruem <zengjiahao2019@163.com>
2026-07-29 10:39:31 +0800

    add runtime=edge to all routes for Cloudflare Pages deployment

---
+++ .vercel/output/static/_next/static/chunks/app/layout-15f70ecaa9e5dc5b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{3673:()=>{},6110:()=>{},8304:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,3673,23)),Promise.resolve().then(n.t.bind(n,6110,23))}},e=>{e.O(0,[440,441,255,358],()=>e(e.s=8304)),_N_E=e.O()}]);
